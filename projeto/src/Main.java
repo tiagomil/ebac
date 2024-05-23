80a3e12ac3a1b210756602110df9a62f6a613a01
tiagomil <tiagomiliszewski@gmail.com>
2024-05-23 20:33:30 -0300

    exercicio alunos

--- projeto/src/Main.java
+++ projeto/src/Main.java
@@ -4,9 +4,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite um numero inteiro: ");
-        int numeroInteiro = Integer.parseInt(scanner.nextLine());
-        System.out.println("Numero inteiro digitado: " + numeroInteiro);
+        System.out.print("Digite a nota 1: ");
+        int nota1 = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Digite a nota 2: ");
+        int nota2 = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Digite a nota 3: ");
+        int nota3 = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Digite a nota 4: ");
+        int nota4 = Integer.parseInt(scanner.nextLine());
+
+        int notaSoma = nota1+nota2+nota3+nota4;
+        int notaFinal = notaSoma/4;
+
+        if(notaFinal >= 7){
+            System.out.println("aprovado");
+        } else if (notaFinal >= 5){
+            System.out.println("recupercao");
+        } else {
+            System.out.println("reprovado");
+        }
+
+
+
+
+
+
+
+
+
         scanner.close();
 
     }
