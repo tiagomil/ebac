83986a8ce47c6512bad2e318b54e47d4a6ef925b
tiagomil <tiagomiliszewski@gmail.com>
2024-05-23 20:45:57 -0300

    ordem alfabetica

--- projeto/src/Main.java
+++ projeto/src/Main.java
@@ -1,41 +1,14 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String[] nomes = {"Tiago", "Mateus", "Paulo"};
 
-        System.out.print("Digite a nota 1: ");
-        int nota1 = Integer.parseInt(scanner.nextLine());
+        Arrays.sort(nomes);
 
-        System.out.print("Digite a nota 2: ");
-        int nota2 = Integer.parseInt(scanner.nextLine());
-
-        System.out.print("Digite a nota 3: ");
-        int nota3 = Integer.parseInt(scanner.nextLine());
-
-        System.out.print("Digite a nota 4: ");
-        int nota4 = Integer.parseInt(scanner.nextLine());
-
-        int notaSoma = nota1+nota2+nota3+nota4;
-        int notaFinal = notaSoma/4;
-
-        if(notaFinal >= 7){
-            System.out.println("aprovado");
-        } else if (notaFinal >= 5){
-            System.out.println("recupercao");
-        } else {
-            System.out.println("reprovado");
+        System.out.println("Nomes em ordem alfabética:");
+        for (String nome : nomes) {
+            System.out.println(nome);
         }
-
-
-
-
-
-
-
-
-
-        scanner.close();
-
     }
 }
