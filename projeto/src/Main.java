e70c1124cbba81a2429ad84ae379cc2e7594ea68
tiagomil <tiagomiliszewski@gmail.com>
2024-05-23 20:12:58 -0300

    mais um exercicio

--- projeto/src/Main.java
+++ projeto/src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Carro carro = new Carro();
-        carro.acelerarCarro();
-        carro.frearCarro();
+        int valor1 = 5;
+        int valor2 = 4;
+        int valor3 = 3;
+        int valor4 = 4;
+        int total = valor1 + valor2 +valor3 +valor4;
+        int media = total / 4;
+
+        System.out.println("A media é: ");
+        System.out.println(media);
+
     }
 }
